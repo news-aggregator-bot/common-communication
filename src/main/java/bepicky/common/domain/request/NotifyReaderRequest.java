9a7a9c8790f600586b4ac04916840efb19e4f5e2
v.yemelianov <v.yemelianov@voverc.com>
2020-09-07 19:59:33 +0700

    notify update

--- src/main/java/bepicky/common/domain/request/NotifyReaderRequest.java
+++ src/main/java/bepicky/common/domain/request/NotifyReaderRequest.java
@@ -1,5 +1,6 @@
 package bepicky.common.domain.request;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,6 +12,7 @@ import java.util.List;
 @NoArgsConstructor
 public class NotifyReaderRequest {
 
+    @JsonProperty("chat_id")
     private long chatId;
 
     private String lang;
