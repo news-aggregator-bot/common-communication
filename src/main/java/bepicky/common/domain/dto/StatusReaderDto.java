a18cae5d9fcf52033e70418e3496b1341a60907d
v.yemelianov <v.yemelianov@voverc.com>
2021-01-03 14:11:22 +0700

    implemented reader status

---
+++ src/main/java/bepicky/common/domain/dto/StatusReaderDto.java
@@ -0,0 +1,40 @@
+package bepicky.common.domain.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Data
+public class StatusReaderDto {
+
+    private String username;
+
+    @JsonProperty("first_name")
+    private String firstName;
+    @JsonProperty("last_name")
+    private String lastName;
+
+    private String status;
+
+    @JsonProperty("primary_language")
+    private LanguageDto primaryLanguage;
+
+    private Set<LanguageDto> languages;
+
+    private Set<CategoryDto> categories;
+
+    private Set<SourceDto> sources;
+
+    @JsonIgnore
+    public Set<CategoryDto> getRegions() {
+        return categories.stream().filter(c -> "REGION".equals(c.getType())).collect(Collectors.toSet());
+    }
+
+    @JsonIgnore
+    public Set<CategoryDto> getCommons() {
+        return categories.stream().filter(c -> "COMMON".equals(c.getType())).collect(Collectors.toSet());
+    }
+}
