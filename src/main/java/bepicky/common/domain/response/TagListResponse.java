797aac2de39efb4e828ea49e99591115b0e40628
v.yemelianov <v.yemelianov@voverc.com>
2021-04-14 08:50:07 +0400

    DEV-31 My Tags button

--- src/main/java/bepicky/common/domain/response/TagListResponse.java
+++ src/main/java/bepicky/common/domain/response/TagListResponse.java
@@ -1,15 +1,30 @@
 package bepicky.common.domain.response;
 
+import bepicky.common.domain.dto.ReaderDto;
 import bepicky.common.domain.dto.TagDto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 import java.util.List;
 
+@EqualsAndHashCode(callSuper = true)
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class TagListResponse {
+public class TagListResponse extends AbstractResponse {
     private List<TagDto> list;
+
+    public TagListResponse(ErrorResponse error) {
+        super(error);
+    }
+
+    public TagListResponse(
+        ReaderDto reader,
+        List<TagDto> list
+    ) {
+        super(reader);
+        this.list = list;
+    }
 }
