2ace793d971f5bd359ca3019382b5710c9e9370a
v.yemelianov <v.yemelianov@voverc.com>
2021-06-02 07:49:43 +0300

    List 5 latest news when subscribed for a tag

--- src/main/java/bepicky/common/domain/response/TagResponse.java
+++ src/main/java/bepicky/common/domain/response/TagResponse.java
@@ -1,20 +1,31 @@
 package bepicky.common.domain.response;
 
+import bepicky.common.domain.dto.NewsNoteDto;
 import bepicky.common.domain.dto.ReaderDto;
 import bepicky.common.domain.dto.TagDto;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Data
 @NoArgsConstructor
 @EqualsAndHashCode(callSuper = true)
 public class TagResponse extends AbstractResponse {
     private TagDto tag;
+    private List<NewsNoteDto> news = new ArrayList<>();
 
     public TagResponse(ReaderDto reader, TagDto tag) {
         super(reader);
         this.tag = tag;
+    }
+
+    public TagResponse(ReaderDto reader, TagDto tag, List<NewsNoteDto> news) {
+        super(reader);
+        this.tag = tag;
+        this.news = news;
     }
 
     public TagResponse(ErrorResponse error) {
